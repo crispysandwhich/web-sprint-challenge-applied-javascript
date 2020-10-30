6e0f38be30862bc812c7d73df3d9fc49f9898c88
de_gentlem4n <th3g3n3lm4n@gmail.com>
2020-10-30 13:24:34 -0400

    made the header

--- components/Header.js
+++ components/Header.js
@@ -10,5 +10,28 @@
 //
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
+const headerContainer = document.querySelector('.header-container');
 
-function Header() {}
+function Header() {
+    
+    const header = document.createElement('div');
+    header.classList.add('header')
+
+    const spanDate = document.createElement('span')
+    spanDate.classList.add('date')
+    spanDate.textContent = 'March 28,2020'
+    header.append(spanDate)
+
+    const h1 = document.createElement('h1')
+    h1.textContent = 'Lambda Times'
+    header.append(h1)
+
+    const spanTemp = document.createElement('span')
+    spanTemp.classList.add('temp')
+    spanTemp.textContent = '98'
+    header.append(spanTemp)
+
+    return header
+}
+
+headerContainer.append(Header())
